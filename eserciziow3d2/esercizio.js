eb4ec600bf466c7d6b7ce845a568bbaefc48537b
ChiaraSoddu <106075562+ChiaraSoddu@users.noreply.github.com>
2022-06-09 17:18:05 +0200

    Add files via upload

---
+++ eserciziow3d2/esercizio.js
@@ -0,0 +1,46 @@
+var numerouno = 10;
+var numerodue = 15;
+
+console.log(numerouno - numerodue);
+console.log(numerouno + numerodue);
+
+var myname = "Giampaolo ";
+var anni = 48;
+var paese = " Caraibi";
+
+console.log(myname + anni + paese);
+
+let name = "Giovanni ";
+
+let cognome = "Tardiola";
+
+console.log(name + cognome);     //perchè è deprecata?
+
+const studio = "Giurisprudenza";
+
+var età;
+
+console.log(età);
+
+var n_uno = 40;
+var n_due = 4;
+
+console.log(n_uno/n_due == 9 ? "true" : "false");
+
+var oggi = "Oggi fa caldo e c'è il sole";
+var ieri = "ieri notte ha piovuto";
+
+var messaggio = `${oggi} ma, al contrario, ${ieri}`;
+
+console.log(messaggio)
+
+const num = 70;
+const nume = 20;
+
+console.log(num == nume ? "true" : "False");
+
+let cibo = null;
+console.log(cibo)
+
+const drink = "cocacola";
+console.log(drink)
